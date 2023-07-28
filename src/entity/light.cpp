a79c23644f2f66b62447005b8f5103df009ba5db
Mateus Elias <meap@cin.ufpe.br>
2023-07-28 19:14:24 -0300

    feat(src/entity/light.cpp): update 'Light' class source code

--- src/entity/light.cpp
+++ src/entity/light.cpp
@@ -1,48 +1,23 @@
 #include <light.h>
 #include <assert.h>
 
-Light::Light(Point location, int red, int green, int blue) {
-    assert(red >= 0 && red <= 255);
-    assert(green >= 0 && green <= 255);
-    assert(blue >= 0 && blue <= 255);
-
+Light::Light(Point location, Color intensity) {
     this->location = location;
-    this->red = red;
-    this->green = green;
-    this->blue = blue;
+    this->intensity = intensity;
 }
 
 Point Light::getLocation() const {
     return location;
 }
 
-int Light::getRed() const {
-    return red;
-}
-
-int Light::getGreen() const {
-    return green;
-}
-
-int Light::getBlue() const {
-    return blue;
+Color Light::getIntensity() const {
+    return intensity;
 }
 
 void Light::setLocation(Point location) {
     this->location = location;
 }
 
-void Light::setRed(int red) {
-    assert(red >= 0 && red <= 255);
-    this->red = red;
-}
-
-void Light::setGreen(int green) {
-    assert(green >= 0 && green <= 255);
-    this->green = green;
-}
-
-void Light::setBlue(int blue) {
-    assert(blue >= 0 && blue <= 255);
-    this->blue = blue;
+void Light::setIntensity(Color intensity) {
+    this->intensity = intensity;
 }
