aebc904fa55c8430b363e32a5752a6cbc7afa1ae
Mateus Elias <meap@cin.ufpe.br>
2023-07-28 02:56:15 -0300

    feat(src/geometry/Point.cpp): adds support to a tridimensional point

--- src/geometry/Point.cpp
+++ src/geometry/Point.cpp
@@ -1,10 +1,14 @@
 #include <Point.h>
 #include <cmath>
 
-Point::Point() : x(0), y(0) {}
+Point::Point() : x(0), y(0), z(0) {}
 
-Point::Point(double x, double y) : x(x), y(y) {}
+Point::Point(double x, double y, double z) : x(x), y(y), z(z) {}
 
 double Point::distance(const Point &other) {
-    return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
+    double x_squared = (x - other.x) * (x - other.x);
+    double y_squared = (y - other.y) * (y - other.y);
+    double z_squared = (z - other.z) * (z - other.z);
+
+    return sqrt(x_squared + y_squared + z_squared);
 }
