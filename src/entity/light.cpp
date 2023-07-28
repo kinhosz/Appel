f418dc48dd8f3ea1c4aab5c93721e4ad5ce50753
Mateus Elias <meap@cin.ufpe.br>
2023-07-28 06:19:01 -0300

    feat(src/entity/light.cpp): defines 'Light' class source code

--- src/entity/light.cpp
+++ src/entity/light.cpp
@@ -1,26 +1,64 @@
 #include <light.h>
+#include <cassert>
 
-Light::Light() : location(Point()), intensity(Intensity()) {}
+Light::Light(Point location, int red, int green, int blue) : location(location), red(red), green(green), blue(blue) {}
 
-Light::Light(Point location, Intensity intensity) : location(location), intensity(intensity) {}
+Light Light::create(Point location, int red, int green, int blue) {
+    assert(red >= 0 && red <= 255);
+    assert(green >= 0 && green <= 255);
+    assert(blue >= 0 && blue <= 255);
 
-int Light::Intensity::clamp(int value) {
-    if (value < 0)
-        return 0;
-    else if (value > 255)
-        return 255;
-    else
-        return value;
+    return Light(location, red, green, blue);
 }
 
-void Light::Intensity::setRed(int r) {
-    red = clamp(r);
+Point Light::get_location() const {
+    return location;
 }
 
-void Light::Intensity::setGreen(int g) {
-    green = clamp(g);
+int Light::get_red() const {
+    return red;
 }
 
-void Light::Intensity::setBlue(int b) {
-    blue = clamp(b);
+int Light::get_green() const {
+    return green;
+}
+
+int Light::get_blue() const {
+    return blue;
+}
+
+int Light::get_rgb() const {
+    return (red << 16) + (green << 8) + blue;
+}
+
+void Light::set_location(Point location) {
+    this->location = location;
+}
+
+void Light::set_red(int red) {
+    assert(red >= 0 && red <= 255);
+
+    this->red = red;
+}
+
+void Light::set_green(int green) {
+    assert(green >= 0 && green <= 255);
+
+    this->green = green;
+}
+
+void Light::set_blue(int blue) {
+    assert(blue >= 0 && blue <= 255);
+
+    this->blue = blue;
+}
+
+void Light::set_rgb(int red, int green, int blue) {
+    assert(red >= 0 && red <= 255);
+    assert(green >= 0 && green <= 255);
+    assert(blue >= 0 && blue <= 255);
+
+    this->red = red;
+    this->green = green;
+    this->blue = blue;
 }
