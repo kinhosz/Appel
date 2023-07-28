8262627fbbd0512d693e6f9485661a5175f8ccb0
José Carlos <39632709+kinhosz@users.noreply.github.com>
2023-07-28 04:55:19 -0300

    feat(vetor): add vetor.h & vetor.cpp (#11)
    * feat(vetor): add vetor.h & vetor.cpp
    
    * style(vetor.cpp): lower_case lib name
    
    * lint: rename to temp name
    
    * lint: rename to lower_case
    
    * chore: n = norm()
    
    * feat(vetor): add rotate
    
    * ref(rotate): extract to method
    
    * fix: add missing parameter
    
    * fix(vetor): temp value passing by reference

---
+++ include/geometry/vetor.h
@@ -0,0 +1,26 @@
+#ifndef GEOMETRY_VETOR_H
+#define GEOMETRY_VETOR_H
+
+struct Vetor{
+private:
+    void rotate(double &x, double &y, double alpha) const;
+public:
+    double x, y, z;
+    Vetor();
+    Vetor(double x, double y, double z);
+    double dot(const Vetor &other) const;
+    Vetor cross(const Vetor &other) const;
+    double angle(const Vetor &other) const;
+    double norm() const;
+    Vetor normalize() const;
+    Vetor rotate_x(double alpha) const;
+    Vetor rotate_y(double alpha) const;
+    Vetor rotate_z(double alpha) const;
+
+    Vetor operator+(Vetor &other) const;
+    Vetor operator-(Vetor &other) const;
+    Vetor operator*(double p) const;
+    Vetor operator/(double p) const;
+};
+
+#endif
---
+++ src/geometry/vetor.cpp
@@ -0,0 +1,76 @@
+#include <vetor.h>
+#include <math.h>
+
+Vetor::Vetor(): x(0), y(0), z(0) {}
+
+Vetor::Vetor(double x, double y, double z): x(x), y(y), z(z) {}
+
+double Vetor::dot(const Vetor &other) const {
+    return x*other.x + y*other.y + z*other.z;
+}
+
+Vetor Vetor::cross(const Vetor &other) const {
+    double i = (y * other.z) - (z * other.y);
+    double j = (z * other.x) - (x * other.z);
+    double k = (x * other.y) - (y * other.x);
+
+    return Vetor(i, j, k);
+}
+
+double Vetor::angle(const Vetor &other) const {
+    double cost = dot(other) / (norm() * other.norm());
+    return acos(cost);
+}
+
+double Vetor::norm() const {
+    return sqrt(x*x + y*y + z*z);
+}
+
+Vetor Vetor::normalize() const {
+    double n = norm();
+    return Vetor(x/n, y/n, z/n);
+}
+
+void Vetor::rotate(double &x, double &y, double alpha) const {
+    double rx = cos(alpha) * x + sin(alpha) * y;
+    double ry = -sin(alpha) * x + cos(alpha) * y;
+
+    x = rx, y = ry;
+}
+
+Vetor Vetor::rotate_x(double alpha) const {
+    double ry = y, rz = z;
+    rotate(ry, rz, alpha);
+
+    return Vetor(x, ry, rz);
+}
+
+Vetor Vetor::rotate_y(double alpha) const {
+    double rx = x, rz = z;
+    rotate(rx, rz, alpha);
+
+    return Vetor(rx, y, rz);
+}
+
+Vetor Vetor::rotate_z(double alpha) const {
+    double rx = x, ry = y;
+    rotate(rx, ry, alpha);
+
+    return Vetor(rx, ry, z);
+}
+
+Vetor Vetor::operator+(Vetor &other) const {
+    return Vetor(x + other.x, y + other.y, z + other.z);
+}
+
+Vetor Vetor::operator-(Vetor &other) const {
+    return Vetor(x - other.x, y - other.y, z - other.z);
+}
+
+Vetor Vetor::operator*(double p) const {
+    return Vetor(x * p, y * p, z * p);
+}
+
+Vetor Vetor::operator/(double p) const {
+    return Vetor(x / p, y / p, z / p);
+}
