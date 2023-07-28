1b8f3936c3ee5e5f071197b9c664fa20a11c9651
Mateus Elias <meap@cin.ufpe.br>
2023-07-28 19:12:50 -0300

    feat(src/graphic): created 'Color' source code

---
+++ src/graphic/color.cpp
@@ -0,0 +1,39 @@
+#include <color.h>
+#include <assert.h>
+
+Color::Color(int red, int green, int blue) {
+    assert(red >= 0 && red <= 255);
+    assert(green >= 0 && green <= 255);
+    assert(blue >= 0 && blue <= 255);
+
+    this->red = red;
+    this->green = green;
+    this->blue = blue;
+}
+
+int Color::getRed() const {
+    return red;
+}
+
+int Color::getGreen() const {
+    return green;
+}
+
+int Color::getBlue() const {
+    return blue;
+}
+
+void Color::setRed(int red) {
+    assert(red >= 0 && red <= 255);
+    this->red = red;
+}
+
+void Color::setGreen(int green) {
+    assert(green >= 0 && green <= 255);
+    this->green = green;
+}
+
+void Color::setBlue(int blue) {
+    assert(blue >= 0 && blue <= 255);
+    this->blue = blue;
+}
