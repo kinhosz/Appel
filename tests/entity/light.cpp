0600b213317446cf1b9c2dafe714dda3831574d6
Mateus Elias <meap@cin.ufpe.br>
2023-07-28 22:19:54 -0300

    feat(tests/entity): add 'Light' validation tests

---
+++ tests/entity/light.cpp
@@ -0,0 +1,21 @@
+#include <entity/light.h>
+#include <geometry/point.h>
+#include <assert.h>
+using namespace std;
+
+int main(){
+    Light l(Point(-1, 2, 10), Color(255, 15, 0));
+
+    assert(l.getLocation() == Point(-1, 2, 10));
+    assert(l.getIntensity().getRed() == 255);
+    assert(l.getIntensity().getGreen() == 15);
+    assert(l.getIntensity().getBlue() == 0);
+
+    l.setLocation(Point(0, 0, 0));
+    l.setIntensity(Color(0, 15, 255));
+
+    assert(l.getLocation() == Point(0, 0, 0));
+    assert(l.getIntensity().getRed() == 0);
+    assert(l.getIntensity().getGreen() == 15);
+    assert(l.getIntensity().getBlue() == 255);
+}
