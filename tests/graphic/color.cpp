96d99d061608bcd8f4b9d7d7b129127ab478ce83
Mateus Elias <meap@cin.ufpe.br>
2023-07-28 23:32:20 -0300

    feat(tests/graphic/color.cpp): import geometry constants

--- tests/graphic/color.cpp
+++ tests/graphic/color.cpp
@@ -1,9 +1,8 @@
 #include <graphic/color.h>
+#include <geometry/constants.h>
 #include <assert.h>
 #include <cmath>
 using namespace std;
-
-const double eps = 1e-6;
 
 int main() {
     Color c1, c2(0, 255, 0);
@@ -27,14 +26,14 @@ int main() {
     assert(c1.getRed() == 255);
     assert(c1.getGreen() == 255);
     assert(c1.getBlue() == 255);
-    assert(abs(c1.getNormRed() - 1.0) < eps);
-    assert(abs(c1.getNormGreen() - 1.0) < eps);
-    assert(abs(c1.getNormBlue() - 1.0) < eps);
+    assert(abs(c1.getNormRed() - 1.0) < EPSILON);
+    assert(abs(c1.getNormGreen() - 1.0) < EPSILON);
+    assert(abs(c1.getNormBlue() - 1.0) < EPSILON);
 
     assert(c2.getRed() == 255);
     assert(c2.getGreen() == 0);
     assert(c2.getBlue() == 0);
-    assert(abs(c2.getNormRed() - 1.0) < eps);
-    assert(abs(c2.getNormGreen() - 0.0) < eps);
-    assert(abs(c2.getNormBlue() - 0.0) < eps);
+    assert(abs(c2.getNormRed() - 1.0) < EPSILON);
+    assert(abs(c2.getNormGreen() - 0.0) < EPSILON);
+    assert(abs(c2.getNormBlue() - 0.0) < EPSILON);
 }
