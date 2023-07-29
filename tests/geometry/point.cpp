12de37428ed3fa6e059623a1fb3a99c51666c2ab
Mateus Elias <meap@cin.ufpe.br>
2023-07-28 21:57:07 -0300

    feat(tests/geometry/point.cpp): add validation tests

--- tests/geometry/point.cpp
+++ tests/geometry/point.cpp
@@ -1,6 +1,15 @@
 #include <geometry/point.h>
+#include <assert.h>
+#include <cmath>
 using namespace std;
 
 int main(){
-    Point p(-2, -1, 0);
+    Point p1(1, 2, 3), p2(4, 5, 6);
+
+    assert(abs(p1.distance(p2) - 5.196152) < 0.000001);
+    assert(p1 == Point(1, 2, 3));
+    assert(p2 == Point(4, 5, 6));
+    assert(p1 != p2);
+    assert(p1 < p2);
+    assert(p2 > p1);
 }
