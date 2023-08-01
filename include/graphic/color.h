975abf13c47b14d083d3fbd0df312145a45b3257
Mateus Elias <meap@cin.ufpe.br>
2023-08-01 01:03:48 -0300

    feat(include/graphic/color.h): modify 'Color' header file to support normalized RGB

--- include/graphic/color.h
+++ include/graphic/color.h
@@ -2,10 +2,21 @@
 #define GRAPHIC_COLOR_H
 
 struct Color {
-    int red, green, blue;
-    
+    enum ColorRepresentation { RGB, NORMALIZED_RGB };
+
+    ColorRepresentation representation;
+    union {
+        struct {
+            int red, green, blue;
+        } rgb;
+        struct {
+            double red, green, blue;
+        } normalized_rgb;
+    };
+
     Color();
-    Color(int red, int green, int blue);
+    Color(int red, int green, int blue, ColorRepresentation representation = RGB);
+    Color(double normRed, double normGreen, double normBlue, ColorRepresentation representation = NORMALIZED_RGB);
 
     int getRed() const;
     int getGreen() const;
@@ -17,6 +28,9 @@ struct Color {
     void setRed(int red);
     void setGreen(int green);
     void setBlue(int blue);
+    void setNormRed(double normRed);
+    void setNormGreen(double normGreen);
+    void setNormBlue(double normBlue);
 };
 
 #endif
