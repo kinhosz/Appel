a5bf28b30dec5a352b44346a4e9d6437f60a8290
Mateus Elias <meap@cin.ufpe.br>
2023-08-04 01:46:52 -0300

    feat(tests/entity/light.cpp): update validation tests

--- tests/entity/light.cpp
+++ tests/entity/light.cpp
@@ -1,10 +1,11 @@
 #include <entity/light.h>
 #include <geometry/point.h>
+#include <graphic/pixel.h>
 #include <assert.h>
 using namespace std;
 
 int main(){
-    Light l(Point(-1, 2, 10), Color(255, 15, 0));
+    Light l(Point(-1, 2, 10), Pixel(255, 15, 0));
 
     assert(l.getLocation() == Point(-1, 2, 10));
     assert(l.getIntensity().getRed() == 255);
@@ -12,7 +13,7 @@ int main(){
     assert(l.getIntensity().getBlue() == 0);
 
     l.setLocation(Point(0, 0, 0));
-    l.setIntensity(Color(0, 15, 255));
+    l.setIntensity(Pixel(0, 15, 255));
 
     assert(l.getLocation() == Point(0, 0, 0));
     assert(l.getIntensity().getRed() == 0);
