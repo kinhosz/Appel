d72dc571cec5669a77572d28ce781d62a16d20a9
Mateus Elias <meap@cin.ufpe.br>
2023-08-04 00:56:13 -0300

    feat(src/graphic/color.cpp): update source code to match with the new header file

--- src/graphic/color.cpp
+++ src/graphic/color.cpp
@@ -1,98 +1,48 @@
 #include <graphic/color.h>
 #include <assert.h>
 
-Color::Color() : representation(RGB), rgb{0, 0, 0} {}
-
-Color::Color(int red, int green, int blue, ColorRepresentation representation)
-    : representation(representation) {
-    assert(red >= 0 && red <= 255);
-    assert(green >= 0 && green <= 255);
-    assert(blue >= 0 && blue <= 255);
-
-    rgb.red = red;
-    rgb.green = green;
-    rgb.blue = blue;
+Color::Color() {
+    red = 0.0;
+    green = 0.0;
+    blue = 0.0;
 }
 
-Color::Color(double normRed, double normGreen, double normBlue, ColorRepresentation representation)
-    : representation(representation) {
-    assert(normRed >= 0.0 && normRed <= 1.0);
-    assert(normGreen >= 0.0 && normGreen <= 1.0);
-    assert(normBlue >= 0.0 && normBlue <= 1.0);
+Color::Color(double red, double green, double blue) {
+    assert(red >= 0.0 && red <= 1.0);
+    assert(green >= 0.0 && green <= 1.0);
+    assert(blue >= 0.0 && blue <= 1.0);
 
-    normalized_rgb.red = normRed;
-    normalized_rgb.green = normGreen;
-    normalized_rgb.blue = normBlue;
+    this->red = red;
+    this->green = green;
+    this->blue = blue;
 }
 
-int Color::getRed() const {
-    assert(representation == RGB);
-    return rgb.red;
+double Color::getRed() const {
+    return red;
 }
 
-int Color::getGreen() const {
-    assert(representation == RGB);
-    return rgb.green;
+double Color::getGreen() const {
+    return green;
 }
 
-int Color::getBlue() const {
-    assert(representation == RGB);
-    return rgb.blue;
+double Color::getBlue() const {
+    return blue;
 }
 
-double Color::getNormRed() const {
-    assert(representation == NORMALIZED_RGB);
-    return normalized_rgb.red;
+void Color::setRed(double red) {
+    assert(red >= 0.0 && red <= 1.0);
+
+    this->red = red;
 }
 
-double Color::getNormGreen() const {
-    assert(representation == NORMALIZED_RGB);
-    return normalized_rgb.green;
+void Color::setGreen(double green) {
+    assert(green >= 0.0 && green <= 1.0);
+
+    this->green = green;
 }
 
-double Color::getNormBlue() const {
-    assert(representation == NORMALIZED_RGB);
-    return normalized_rgb.blue;
-}
+void Color::setBlue(double blue) {
+    assert(blue >= 0.0 && blue <= 1.0);
 
-void Color::setRed(int red) {
-    assert(representation == RGB);
-    assert(red >= 0 && red <= 255);
-    representation = RGB;
-    rgb.red = red;
-}
-
-void Color::setGreen(int green) {
-    assert(representation == RGB);
-    assert(green >= 0 && green <= 255);
-    representation = RGB;
-    rgb.green = green;
-}
-
-void Color::setBlue(int blue) {
-    assert(representation == RGB);
-    assert(blue >= 0 && blue <= 255);
-    representation = RGB;
-    rgb.blue = blue;
-}
-
-void Color::setNormRed(double normRed) {
-    assert(representation == NORMALIZED_RGB);
-    assert(normRed >= 0.0 && normRed <= 1.0);
-    representation = NORMALIZED_RGB;
-    normalized_rgb.red = normRed;
-}
-
-void Color::setNormGreen(double normGreen) {
-    assert(representation == NORMALIZED_RGB);
-    assert(normGreen >= 0.0 && normGreen <= 1.0);
-    representation = NORMALIZED_RGB;
-    normalized_rgb.green = normGreen;
-}
-
-void Color::setNormBlue(double normBlue) {
-    assert(representation == NORMALIZED_RGB);
-    assert(normBlue >= 0.0 && normBlue <= 1.0);
-    representation = NORMALIZED_RGB;
-    normalized_rgb.blue = normBlue;
+    this->blue = blue;
 }
