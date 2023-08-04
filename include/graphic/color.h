a06dc1582b2dc3365fa18b37e30b867c635431cc
Mateus Elias <meap@cin.ufpe.br>
2023-08-04 00:55:50 -0300

    feat(include/graphic/color.h): update header file structure

--- include/graphic/color.h
+++ include/graphic/color.h
@@ -2,35 +2,18 @@
 #define GRAPHIC_COLOR_H
 
 struct Color {
-    enum ColorRepresentation { RGB, NORMALIZED_RGB };
-
-    ColorRepresentation representation;
-    union {
-        struct {
-            int red, green, blue;
-        } rgb;
-        struct {
-            double red, green, blue;
-        } normalized_rgb;
-    };
+    double red, green, blue;
 
     Color();
-    Color(int red, int green, int blue, ColorRepresentation representation = RGB);
-    Color(double normRed, double normGreen, double normBlue, ColorRepresentation representation = NORMALIZED_RGB);
+    Color(double red, double green, double blue);
 
-    int getRed() const;
-    int getGreen() const;
-    int getBlue() const;
-    double getNormRed() const;
-    double getNormGreen() const;
-    double getNormBlue() const;
+    double getRed() const;
+    double getGreen() const;
+    double getBlue() const;
 
-    void setRed(int red);
-    void setGreen(int green);
-    void setBlue(int blue);
-    void setNormRed(double normRed);
-    void setNormGreen(double normGreen);
-    void setNormBlue(double normBlue);
+    void setRed(double red);
+    void setGreen(double green);
+    void setBlue(double blue);
 };
 
 #endif
