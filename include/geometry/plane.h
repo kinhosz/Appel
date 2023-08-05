97ef4d14fc4403250a38832ddd577cd305859694
Mateus Elias <meap@cin.ufpe.br>
2023-08-05 00:10:45 -0300

    feat(include/geometry): add 'Plane' header file

---
+++ include/geometry/plane.h
@@ -0,0 +1,27 @@
+#ifndef GEOMETRY_PLANE_H
+#define GEOMETRY_PLANE_H
+
+#include <geometry/box.h>
+#include <geometry/point.h>
+#include <geometry/vetor.h>
+#include <graphic/color.h>
+
+class Plane : public Box {
+private:
+    Point point;
+    Vetor normalVector;
+    Color color;
+public:
+    Plane();
+    Plane(Point point, Vetor normalVector, Color color, double kd, double ks, double ka, double kr, double kt, double roughness);
+
+    Point getPoint() const;
+    Vetor getNormalVector() const;
+    Color getColor() const;
+
+    void setPoint(Point point);
+    void setNormalVector(Vetor normalVector);
+    void setColor(Color color);
+};
+
+#endif
