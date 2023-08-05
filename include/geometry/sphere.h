d98e42ba8f4d0611d5aaaf0f15b1d3b9761c0248
Mateus Elias <meap@cin.ufpe.br>
2023-08-04 22:57:56 -0300

    feat(include/geometry): add 'Sphere' header file

---
+++ include/geometry/sphere.h
@@ -0,0 +1,25 @@
+#ifndef GEOMETRY_SPHERE_H
+#define GEOMETRY_SPHERE_H
+
+#include <geometry/box.h>
+#include <geometry/point.h>
+
+class Sphere : public Box {
+private:
+    Point center;
+    double radius;
+    Color color;
+public:
+    Sphere();
+    Sphere(Point center, double radius, Color color, double kd, double ks, double ka, double kr, double kt, double roughness);
+
+    Point getCenter() const;
+    double getRadius() const;
+    Color getColor() const;
+
+    void setCenter(Point center);
+    void setRadius(double radius);
+    void setColor(Color color);
+};
+
+#endif
