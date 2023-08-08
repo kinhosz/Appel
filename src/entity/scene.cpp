afdd0e656465ddee1d0665d5e480709a68353c39
Mateus Elias <meap@cin.ufpe.br>
2023-08-07 23:20:58 -0300

    feat(src/entity/scene.cpp): update source code to match with the new header file

--- src/entity/scene.cpp
+++ src/entity/scene.cpp
@@ -1,10 +1,31 @@
 #include <entity/scene.h>
+#include <assert.h>
 
-Scene::Scene() : lights(std::vector<Light>()), environmentColor(Color()) {}
+Scene::Scene() {
+    this->lights = std::map<int, Light>();
+    this->lightsCurrentIndex = this->lights.size() - 1;
+    this->environmentColor = Color(0, 0, 0);
+    this->objects = std::map<int, Box>();
+    this->objectsCurrentIndex = this->objects.size() - 1;
+}
 
-Scene::Scene(std::vector<Light> lights, Color environmentColor) : lights(lights), environmentColor(environmentColor) {}
+Scene::Scene(const Color& environmentColor) {
+    this->lights = std::map<int, Light>();
+    this->lightsCurrentIndex = this->lights.size() - 1;
+    this->environmentColor = environmentColor;
+    this->objects = std::map<int, Box>();
+    this->objectsCurrentIndex = this->objects.size() - 1;
+}
 
-std::vector<Light> Scene::getLights() const {
+Scene::Scene(const std::map<int, Light>& lights, const Color& environmentColor, const std::map<int, Box>& objects) {
+    this->lights = lights;
+    this->lightsCurrentIndex = this->lights.size() - 1;
+    this->environmentColor = environmentColor;
+    this->objects = objects;
+    this->objectsCurrentIndex = this->objects.size() - 1;
+}
+
+std::map<int, Light> Scene::getLights() const {
     return this->lights;
 }
 
@@ -12,10 +33,32 @@ Color Scene::getEnvironmentColor() const {
     return this->environmentColor;
 }
 
-void Scene::setLights(std::vector<Light> lights) {
-    this->lights = lights;
+std::map<int, Box> Scene::getObjects() const {
+    return this->objects;
 }
 
-void Scene::setEnvironmentColor(Color environmentColor) {
+int Scene::addLight(const Light& light) {
+    this->lightsCurrentIndex++;
+    this->lights[lightsCurrentIndex] = light;
+    return lightsCurrentIndex;
+}
+
+void Scene::removeLight(int index) {
+    assert(this->lights.find(index) != this->lights.end());
+    this->lights.erase(index);
+}
+
+void Scene::setEnvironmentColor(const Color& environmentColor) {
     this->environmentColor = environmentColor;
+}
+
+int Scene::addObject(const Box& object) {
+    this->objectsCurrentIndex++;
+    this->objects[objectsCurrentIndex] = object;
+    return objectsCurrentIndex;
+}
+
+void Scene::removeObject(int index) {
+    assert(this->objects.find(index) != this->objects.end());
+    this->objects.erase(index);
 }
