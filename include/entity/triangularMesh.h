c91d81a0e33258fdcf66277012267badd1a49eb8
Filipe Eduardo <filipeed14@gmail.com>
2023-08-09 13:32:38 -0300

    Apply suggestions from code review
    Co-authored-by: Mateus Elias <meap@cin.ufpe.br>

--- include/entity/triangularMesh.h
+++ include/entity/triangularMesh.h
@@ -1,19 +1,18 @@
-#ifndef ENTITY_TRIANGLEMESH_H
-#define ENTITY_TRIANGLEMESH_H
+#ifndef ENTITY_TRIANGULARMESH_H
+#define ENTITY_TRIANGULARMESH_H
 
 #include <array>
 #include <vector>  
+#include <entity/box.h>
 #include <graphic/color.h>
-#include <geometry/surfaceIntersection.h>
-#include <geometry/ray.h>
-#include <entity/box.h>  
+#include <geometry/point.h>
+#include <geometry/vetor.h>
 
 class TriangularMesh : public Box {
 private:
-    int numTriangles;
-    int numVertices;
+    int numberOfTriangles;
+    int numberOfVertices;
     std::vector<Point> vertices;
-    std::vector<Vetor> normals;
     std::vector<std::array<int, 3>> triangleIndices;
     std::vector<Vetor> triangleNormals;
     std::vector<Vetor> vertexNormals;
@@ -21,7 +20,21 @@ private:
 
 public:
     TriangularMesh();
-    TriangularMesh(double kd, double ks, double ka, double kr, double kt, double roughness);
+    TriangularMesh(
+        int numberOfTriangles,
+        int numberOfVertices,
+        std::vector<Point> vertices,
+        std::vector<std::array<int, 3>> triangles,
+        std::vector<Vetor> triangleNormals,
+        std::vector<Vetor> vertexNormals,
+        std::vector<Color> colors,
+        double kd,
+        double ks,
+        double ka,
+        double kr,
+        double kt,
+        double roughness
+    );
 };
 
 #endif
