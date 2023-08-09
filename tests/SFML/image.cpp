29490ed1f3a46cf72340714d8e1c0141c88196ae
kinhosz <jcsc@cin.ufpe.br>
2023-08-08 21:10:23 -0300

    feat(sfml): test for save image

--- tests/SFML/image.cpp
+++ tests/SFML/image.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <cassert>
 
 int main() {
     sf::Image image;
@@ -9,10 +10,11 @@ int main() {
     sf::Color color(255, 0, 0); // Cor vermelha (R=255, G=0, B=0)
     image.setPixel(x, y, color);
 
-    if (image.saveToFile("output.png")) {
+    if (image.saveToFile("bin/output.png")) {
         // Imagem salva com sucesso
     } else {
         // Ocorreu um erro ao salvar a imagem
+        assert(false);
     }
 
     return 0;
