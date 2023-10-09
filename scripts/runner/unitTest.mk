a9b1c6fa268b670f4f68549e7e8b1fd42f411486
kinhosz <jcsc@cin.ufpe.br>
2023-10-09 18:23:42 -0300

    fix(worflow): running using CPU

--- scripts/runner/unitTest.mk
+++ scripts/runner/unitTest.mk
@@ -1,5 +1,5 @@
 # make unit FNAME=./../NAME_OF_FILE.cpp
-unit: build_bin
+unit: build_bin compile_cpp compile_cuda
 	$(call compile,$(FNAME),example)
 	$(call print_message,$(FNAME))
 	$(call run,example.exe)
