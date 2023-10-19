d237be0f04c715dc72caf6c4a3e093362bddaf6b
kinhosz <jcsc@cin.ufpe.br>
2023-10-18 23:40:34 -0300

    fix: need analysis

--- src/geometry/surfaceIntersection.cpp
+++ src/geometry/surfaceIntersection.cpp
@@ -18,7 +18,7 @@ SurfaceIntersection::SurfaceIntersection(
 
 Vetor SurfaceIntersection::getReflection(const Vetor &direction) const {
     Vetor tNormal = normal.normalize();
-    Vetor tDirection = direction.normalize();
+    Vetor tDirection = normal.normalize();
 
     return (Vetor(tNormal * (2.0 * tNormal.dot(tDirection))) - tDirection).normalize();   
 }
